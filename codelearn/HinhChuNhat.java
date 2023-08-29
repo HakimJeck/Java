73c36443f529abc3941fc466886ad64ce8544e9d
HakimJeck <120010470+HakimJeck@users.noreply.github.com>
2023-08-29 19:21:03 +0700

    java

---
+++ codelearn/HinhChuNhat.java
@@ -0,0 +1,29 @@
+package codelearn;
+
+import java.util.Scanner;
+
+public class HinhChuNhat {
+    public static void main(String[]args){
+        HinhChuNhat1 HCN = new HinhChuNhat();
+        HCN.getInformation();
+        HCN.display;
+    }
+}
+class HinhChuNhat1 {
+    double length, width;
+    public void getInformation(){
+        Scanner sc = new Scanner(System.in);
+        length = sc.nextDouble();
+        width = sc.nextDouble();
+    }
+    public double getArea(){
+        return length * width;
+    }
+    public double getPerimeter(){
+        return ( length + width)*2
+    }
+    public void display(){
+        System.out.println("Area:" + getArea());
+        System.out.println("Perimeter " + getPerimeter());
+    }
+}
